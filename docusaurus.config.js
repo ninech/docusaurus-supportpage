3becfef5f7780300260d3cf8f7cf0c566ca92f59
Simone Piccolo <simip@nine.ch>
2022-10-28 11:32:00 +0200

    feat: changing deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ninech', // Usually your GitHub org/user name.
   projectName: 'docusaurus-supportpage', // Usually your repo name.
-  deploymentBranch: 'production', 
+  deploymentBranch: 'gh-pages', 
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
