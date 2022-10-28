ad576b530672343e9208fe17bbb6bcf528fee580
Simone Piccolo <simip@nine.ch>
2022-10-28 10:42:33 +0200

    changing baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-supportpage/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
